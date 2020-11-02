39e4dd293bf382c2ed934e942e309545aa4305cc
Manuela Munoz <manuelamunoz103@gmail.com>
2020-11-01 19:36:38 -0500

    Query

--- EmployeeSQL/employees_information.sql
+++ EmployeeSQL/employees_information.sql
@@ -11,11 +11,11 @@ SELECT * FROM titles
 CREATE TABLE employees (
     emp_no INT  NOT NULL ,
     emp_title_id VARCHAR(5) NOT NULL ,
-    birth_date VARCHAR(11)  NOT NULL ,
+    birth_date DATE  NOT NULL ,
     first_name VARCHAR(15)  NOT NULL ,
     last_name VARCHAR(20)  NOT NULL ,
     sex VARCHAR(1)  NOT NULL ,
-    hire_date VARCHAR(11)  NOT NULL ,
+    hire_date DATE NOT NULL ,
     PRIMARY KEY (emp_no),
 	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
 );
